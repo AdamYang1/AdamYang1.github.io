63394acb979f6429e72f71a5a225e1f1e2df21d8
Shixiang Yang <adam_yang2021@163.com>
2022-07-01 23:38:12 -0700

    Create skill.js

---
+++ assets/js/skill.js
@@ -0,0 +1,41 @@
+function displayFront() {
+	var front = document.getElementById("front");
+	// front.style.backgroundColor = "#18bfef";
+	front.style.display = "block";
+}
+
+function stopDisplayFront() {
+	var front = document.getElementById("front");
+	// front.style.backgroundColor = "#18bfef";
+	front.style.display = "none";
+}
+
+function displayBack() {
+	var back = document.getElementById("back");
+	back.style.display = "block";
+}
+
+function stopDisplayBack() {
+	var back = document.getElementById("back");
+	back.style.display = "none";
+}
+
+function displayData() {
+	var data = document.getElementById("data");
+	data.style.display = "block";
+}
+
+function stopDisplayData() {
+	var data = document.getElementById("data");
+	data.style.display = "none";
+}
+
+function displayGeneral() {
+	var general = document.getElementById("general");
+	general.style.display = "block";
+}
+
+function stopDisplayGeneral() {
+	var general = document.getElementById("general");
+	general.style.display = "none";
+}
